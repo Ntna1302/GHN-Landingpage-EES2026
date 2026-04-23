f22b2ae5c832c45f80fcb5685aeadf5a893bf232
Ntna1302 <ntna010302@gmail.com>
2026-04-23 23:15:49 +0700

    chanaaa

--- components/sections/GroupFinder.tsx
+++ components/sections/GroupFinder.tsx
@@ -9,7 +9,7 @@ const GROUPS = [
     num: "01",
     code: "Nhóm 1A", 
     name: "NV Giao nhận",
-    detail: "NVPTTT, NVGH\nLàm khảo sát qua App Driver hoặc quét QR tại bưu cục / kho xuất phát.",
+    detail: "NVPTTT, NVGH (GTX)\nLàm khảo sát qua App Driver hoặc quét QR tại bưu cục / kho xuất phát.",
     method: "App Driver + QR tại bưu cục",
     date: "12/05 – 20/05/2026",
     start: "2026-05-12",
